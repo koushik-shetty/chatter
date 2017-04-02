55c23de643f2c55f64aac94820483fd6c9dab257
Koushik Shetty <kou4307@gmail.com>
2017-04-02 11:12:38 +0530

    initial commit

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+/**
+ * Client Entry File
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>Hello World!</div>,
+    document.getElementById('app')
+)
---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+/**
+ * Server Entry File
+ */
+
+import express from 'express'
+
+process.chdir(__dirname)
+
+express()
+    .use(express.static('./static'))
+    .use(express.static('../static'))
+    .get('*', (req, res) => {
+        res.type('html').end(template('Hello'))
+    })
+    .listen(8888, () => console.log('App is listening on http://localhost:8888/'))
+
+
+function template (body = '') {
+return `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fullstack Challenge</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/normalize.css" rel="stylesheet">
+</head>
+<body>
+</body>
+    <div id='app'>${body}</div>
+    <script src="/client.js"></script>
+</html>
+`
+}
