ac7aab1202db5ee149382c8bc8647fa878e48748
Koushik Shetty <kou4307@gmail.com>
2017-04-05 23:43:14 +0530

    integrate the server code

--- src/server/index.js
+++ src/server/index.js
@@ -2,29 +2,54 @@
  * Server Entry File
  */
 
-import express from 'express'
+import express from 'express';
+import engine from 'engine.io';
+
+import Worker from './handlers/Worker';
+import { Type } from '../constants';
 
 process.chdir(__dirname)
 
-express()
+const socketPort = process.env.SOCKETPORT || 8889;
+const serverPort = process.env.SERVERPORT || 8888;
+
+let exp = express()
     .use(express.static('./static'))
     .use(express.static('../static'))
     .get('*', (req, res) => {
         res.type('html').end(template('Hello'))
-    })
-    .listen(8888, () => console.log('App is listening on http://localhost:8888/'))
+    });
+
+let server = engine.listen(socketPort, undefined, () => { console.log(`socket listening at: ${socketPort}`) });
+server.attach(exp);
+const workerFunc = Worker(server);
+server.on('connection', (socket) => {
+    
+    socket.on('message', (data) => {
+        workerFunc(JSON.parse(data), socket);
+    });
+
+    socket.on('close', () => {
+        workerFunc({ type: Type.UnRegister }, socket)
+    });
+});
+
+exp.listen(serverPort, () => console.log(`App is listening on http://localhost:${serverPort}/`));
 
 
-function template (body = '') {
-return `
+
+function template(body = '') {
+    return `
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Fullstack Challenge</title>
+    <title>Chatter</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/normalize.css" rel="stylesheet">
+    <link href="/main.css" rel="stylesheet">
+    <link rel="icon" href="assests/login/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
 </body>
